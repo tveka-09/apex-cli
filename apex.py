c3590384bafe09247799d991f0c2027b3f7b028b
tveka-09 <tveka-09.skalen@icloud.com>
2023-01-06 22:03:28 +0100

    consolidated

--- apex.py
+++ apex.py
@@ -10,6 +10,13 @@ import sys
 import mysql.connector
 from time import sleep
 from collections import Counter
+import pathlib
+
+key = '/home/apex/protected/key.txt'
+
+tempoutput = '/home/apex/apex-cli/tempoutput.txt'
+
+googlemaps = "https://maps.googleapis.com/maps/api/distancematrix/xml?origins="
 
 program_home = '/home/apex/apex-cli/'
 
@@ -21,18 +28,18 @@ system_name = " Apex "
 
 version = "Version 1.0 "
 
+with open(key) as f:
+    KEY = f.readline()
+    f.close
+
 PURPLE = '\033[95m'
 CYAN = '\033[96m'
-DARKCYAN = '\033[36m'
 BLUE = '\033[94m'
 GREEN = '\033[92m'
-YELLOW = '\033[93m'
 RED = '\033[91m'
 BOLD = '\033[1m'
-UNDERLINE = '\033[4m'
 END = '\033[0m'
 HEADER = '\033[95m'
-OKBLUE = '\033[94m'
 OKCYAN = '\033[96m'
 OKGREEN = '\033[92m'
 WARNING = '\033[93m'
@@ -47,18 +54,63 @@ def Logo():
     for _ in range(num):
         os.system('clear')
         print(''), print('')
-        print (BOLD + system_name+version)
+        print (BOLD + WHITE + system_name+version)
         print(''), print('')
         sleep(0.1)
         os.system('clear')
         print(''), print('')
-        print (WHITE + system_name+version)
+        print (RED + system_name+version)
         print(''), print('')
         sleep(0.1)
 Logo()
 
-exec(open(program_home + collector).read())
+# MENY
+def m_collector():
+    print('\nRunning Collector \n')
+    datum = input("Datum: ")
+    start = input("Startadress: ")
+    stopp = input("Stoppadress: ")
+    spec = input("T & R? (Ja / Nej) ")
+    url = ""+googlemaps+""+start+"&destinations="+stopp+"&departure_time=now&key="+KEY+""
 
-exec(open(program_home + parsing).read())
+    payload={}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    with open(tempoutput, 'w') as f:
+        f.write('' +datum + '\n' +start + '\n' +stopp + '\n' + (str(response.text)) +spec + '\n')
+        f.close
+    input('\nPush enter to retun to menu')
+
+def m_parsing():
+    print('\nRunning Parsing \n')
+    exec(open(program_home + parsing).read())
+    input('\nPush enter to retun to menu')
+
+def show_menu():
+    print ('\n1) Collector')
+    print ('2) Parsing')
+    print ('Q) Exit\n')
+
+def menu():
+    while True:
+        show_menu()
+        choice = input('Enter your choice: ').lower()
+        if choice == '1':
+            m_collector()
+        elif choice == '2':
+            m_parsing()
+        elif choice == 'q':
+            file = pathlib.Path(tempoutput)
+            if file.exists ():
+                os.remove(tempoutput)
+                return
+            return
+        else:
+            print(f'Not a correct choice: <{choice}>,try again')
+
+if __name__ == '__main__':
+    menu()
 
 
