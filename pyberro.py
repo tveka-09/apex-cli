e0991da52e91bab2783750e462832104b4347495
Michael Bellander <120797732+tveka-09@users.noreply.github.com>
2023-01-11 22:51:24 +0100

    New file

---
+++ pyberro.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+###########################
+# Berromator Technologies #
+#    Michael Bellander    #
+#    michael@berro.se     #
+###########################
+
+from colored import fg, bg, attr
+
+snow2 = fg('#EEE9E9')
+snow3 = fg('#CDC9C9')
+sgigray72 = fg('#B7B7B7')
+plum1 = fg('#FFBBFF')
+lightskyblue = fg('#87CEFA')
+deepskyblue1 = fg('#00BFFF')
+cornflowerblue = fg('#6495ED')
+darkolivegreen1 = fg('#CAFF70')
+darkseagreen1 = fg('#C1FFC1')
+
+#pinkpalette
+pinkp1 = fg('#ffcfe8')
+pinkp2 = fg('#ffa0d2')
+pinkp3 = fg('#ff62b4')
+pinkp4 = fg('#ff1790')
+pinkp5 = fg('#c70068')
+
+# bggreenpal
+bggreenpal1 = bg('#ecffe6')
+bggreenpal2 = bg('#d9ffcc')
+bggreenpal3 = bg('#9fff80')
+bggreenpal4 = bg('#66ff33')
+bggreenpal5 = bg('#53ff1a')
+bggreenpal6 = bg('#40ff00')
+bggreenpal7 = bg('#39e600')
+bggreenpal8 = bg('#33cc00')
+bggreenpal9 = bg('#269900')
+bggreenpal10 = bg('#1a6600')
+bggreenpal11 = bg('#134d00')
+bggreenpal12 = bg('#0d3300')
+bggreenpal13 = bg('#061a00')
+
+
+# qwertyowengreen
+qwertyowengreen1 = fg('#d1fed4')
+qwertyowengreen2 = fg('#9af5a0')
+qwertyowengreen3 = fg('#58e562')
+qwertyowengreen4 = fg('#33cc3f')
+qwertyowengreen5 = fg('#1d9526')
+qwertyowengreen6 = fg('#5cc928')
+
+res = attr('reset')
+
